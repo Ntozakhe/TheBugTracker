b7133b252f1d68e376865629651d3cc0c8d6cdf4
KennyNtozakhe <mpentozakhe@gmail.com>
2024-07-04 12:25:16 +0200

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a Max of {1} charactors")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a Max of {1} charactors")]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+
+    }
+}
